80d2510c643ce51887cd28c02c449697e14d2847
KopLaim <mail@mail.com>
2025-12-02 14:07:00 +0500

    Создан каркас кода

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
+void ReadFromFile(){
+
+}
+void ShowOnScreen(){
+
+}
+
+void WriteToFile(){
+
+}
+
 int main()
 {
+
+    ReadFromFile();
+    ShowOnScreen();
+    WriteToFile();
+
     cout << "Hello world!" << endl;
     return 0;
 }
