d6de430f8724b6070b9c305fffe3d7beed282848
Kristina <kris.podgornova@yandex.ru>
2025-12-03 16:15:49 +0500

    Создана функция для записи информации в файл

--- main.cpp
+++ main.cpp
@@ -12,16 +12,28 @@ void ShowOnScreen(){
 
 }
 
-void WriteToFile(){
-
+void WriteToFile(const vector<string>& actors, const string& filename) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        for (const auto& actor : actors) {
+            file << actor << endl;
+        }
+        file.close();
+        cout << "Infomation write to file: " << filename << endl;
+    } else {
+        cout << "Error: " << filename << endl;
+    }
 }
 
 int main()
 {
 
+
+
+    const string outputFile = "actors_output.txt";
+
     ReadFromFile();
     ShowOnScreen();
-    WriteToFile();
 
     cout << "Hello world!" << endl;
     return 0;
